c5c6dd7d84c7ea9ab3e6d4d27f340488e2669740
louie <ouseiketsu@protonmail.com>
2023-02-28 23:11:03 +0800

    feat:文件下载接口;

--- src/main/java/com/reggie/controller/CommonController.java
+++ src/main/java/com/reggie/controller/CommonController.java
@@ -2,12 +2,16 @@ package com.reggie.controller;
 
 import com.reggie.common.JsonResponse;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.UUID;
 
@@ -41,5 +45,26 @@ public class CommonController {
             e.printStackTrace();
         }
         return JsonResponse.success(fileName);
+    }
+
+    @GetMapping("/download")
+    // 报错： 写了post了
+    // 这个没有return
+    public void downloadFile(String name, HttpServletResponse response) {
+        byte[] readData = new byte[1024];
+        int len;
+        try {
+            FileInputStream fileInputStream = new FileInputStream(new File(basePath + name));
+            // 这个os无法直接new出来;
+            ServletOutputStream outputStream = response.getOutputStream();
+            while ((len = fileInputStream.read(readData)) != -1) { // 这里我把len给忘记了
+                outputStream.write(readData, 0, len);
+                outputStream.flush();
+            }
+            fileInputStream.close();
+            outputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
